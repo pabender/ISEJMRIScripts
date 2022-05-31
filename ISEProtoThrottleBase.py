c2451a73805dd1d01213a242ec45f5b6c985d825
Paul Bender <paul.bender@acm.org>
2022-05-31 00:19:08 -0400

    comment out success message

--- ISEProtoThrottleBase.py
+++ ISEProtoThrottleBase.py
@@ -251,7 +251,7 @@ class ProtoThrottleHeartBeat(jmri.jmrit.automat.AbstractAutomaton):
       try:
          self.Xbee.setReceiveTimeout(2000)
          self.Xbee.sendBroadcastData(txBuffer)
-         print 'status message sent'
+         # print 'status message sent'
       except:
          print 'failed to send heartbeat'
          pass
